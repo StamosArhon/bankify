f7659acf2b9271e4af0ba0440fcdcdc24d69804c
Stamos Archontis <sarchontis@factreview.gr>
2026-04-11 18:11:37 +0300

    Phase 1: protect login secrets

--- android/app/src/main/kotlin/com/dreautall/waterflyiii/MainActivity.kt
+++ android/app/src/main/kotlin/com/dreautall/waterflyiii/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.dreautall.waterflyiii
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
 }
